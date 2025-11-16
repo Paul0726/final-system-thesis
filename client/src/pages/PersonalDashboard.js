adc5663e2748d184e201b656a3c157537fd2fdce
paul <dequillajpc967@gmail.com>
2025-11-16 23:28:52 +0800

    Fix unused import error in PersonalDashboard.js

--- client/src/pages/PersonalDashboard.js
+++ client/src/pages/PersonalDashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useSearchParams, Link, useNavigate } from 'react-router-dom';
+import { useSearchParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import './PersonalDashboard.css';
 
